d4601b00bdb7fd1477ae0e40894bffd9671a49ec
David Clamage <rangsk@gmail.com>
2019-02-13 00:27:59 -0800

    CSS Prettyness Added chart of paragon level over time

--- paragon1.1.js
+++ paragon1.1.js
@@ -17,3 +17,14 @@ function DiffParagon(a, b) {
     var h = Math.max(a, b);
     return BigNumber(paragons.paragon[h].total).minus(BigNumber(paragons.paragon[l].total));
 }
+
+function GetParagonLevel(xp) {
+    var xpInt = xp.toNumber()
+    for (var i = 0; i < 10000; i++) {
+        var xpTotal = paragons.paragon[i].total;
+        if (BigNumber(xpTotal).isGreaterThan(xp)) {
+            return i - 1;
+        }
+    }
+    return 10000;
+}
